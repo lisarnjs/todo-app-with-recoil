4c78bebd51e0158064c872a32733a2805fe8d4ec
권은서 <kwonenseo@gmail.com>
2022-05-19 13:40:44 +0900

    feat: ToDo 카테고리 수정

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,34 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text, category }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory: IToDo["category"]) => {
+    setToDos((oldToDos) => {
+      // 수정할 ToDo index 찾기
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      // 수정할 ToDo의 category 수정
+      const newToDo = { text, id, category: newCategory };
+      // slice()를 통해 기존 배열과 새 배열을 합침
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      {category !== "DOING" && <button>Doing</button>}
-      {category !== "TO_DO" && <button>To Do</button>}
-      {category !== "DONE" && <button>Done</button>}
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
